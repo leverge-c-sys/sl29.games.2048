749f145956c34298a0b948ebe0b894ea9201dea8
Guest <guest-ayfnnt@w-template-bios.nsi.lan>
2026-01-22 15:05:57 +0100

    implémentation de _creer_plateau_vide -> OK

--- src/sl29/games/_2048.py
+++ src/sl29/games/_2048.py
@@ -43,6 +43,14 @@ def _creer_plateau_vide() -> List[List[int]]:
     :return: Une grille vide.
     :rtype: List[List[int]]
     """
+    grille = []
+    for _ in range (TAILLE):
+        ligne = []
+        for _ in range(TAILLE) :
+            ligne.append(0)
+        grille.append(ligne)
+    return grille
+
     raise NotImplementedError("Fonction _creer_plateau_vide non implémentée.")
 
 def _get_cases_vides(plateau: List[List[int]]) -> List[Tuple[int, int]]:
